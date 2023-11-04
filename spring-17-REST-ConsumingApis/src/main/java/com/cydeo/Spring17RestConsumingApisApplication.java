a89db370fe051e5d2c6052ddc5144da6830073b9
selimdemirer <selimmdemirerr@gmail.com>
2023-11-04 07:47:33 +0300

    Consuming APIs is completed

--- spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
+++ spring-17-REST-ConsumingApis/src/main/java/com/cydeo/Spring17RestConsumingApisApplication.java
@@ -1,5 +1,6 @@
 package com.cydeo;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -17,6 +18,11 @@ public class Spring17RestConsumingApisApplication {
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    public ModelMapper mapper() {
+        return new ModelMapper();
     }
 
 }
