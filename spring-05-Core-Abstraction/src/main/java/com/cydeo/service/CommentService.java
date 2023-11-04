3d76f0bfc3f7a9cb0ee58cfa70d881c601450ee0
SELIM DEMIRER <125173819+selimdemirer@users.noreply.github.com>
2023-11-04 06:18:21 +0300

    spring-05-Core-Abstraction

---
+++ spring-05-Core-Abstraction/src/main/java/com/cydeo/service/CommentService.java
@@ -0,0 +1,34 @@
+package com.cydeo.service;
+
+import com.cydeo.model.Comment;
+
+import com.cydeo.proxy.CommentNotificationProxy;
+import com.cydeo.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentService {
+
+    //Using "final" is good practise! Because it is warning us to create constructor
+    private final CommentRepository commentRepository; // We need Has A relationship!
+    private final CommentNotificationProxy commentNotificationProxy; // We need Has A relationship!
+
+    // No need for @Autowired, because we have one constructor
+    public CommentService(CommentRepository commentRepository, @Qualifier("EMAIL") CommentNotificationProxy commentNotificationProxy) {
+        this.commentRepository = commentRepository;
+        this.commentNotificationProxy = commentNotificationProxy;
+    }
+
+    public void publishComment(Comment comment){
+
+        //save in the DB
+        commentRepository.storeComment(comment); // In order to use this method, we created Has A relationship using class (interface -> loosely coupled) name above fields!
+
+        //send email
+        commentNotificationProxy.sendComment(comment); // In order to use this method, we created Has A relationship using class (interface -> loosely coupled) name above fields!
+
+    }
+
+}
