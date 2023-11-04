2296cbe4264299655e5f8c8b5da634e3c26e3ef2
selimdemirer <selimmdemirerr@gmail.com>
2023-11-04 08:26:16 +0300

    Module-16 Json Annotations are done!

--- spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/dto/AccountDTO.java
@@ -1,12 +1,18 @@
 package com.cydeo.dto;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+//@JsonIgnoreProperties(value = {"address","country"}, ignoreUnknown = true)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountDTO {
 
+    @JsonIgnore
     private String name;
     private String address;
     private String country;
@@ -14,6 +20,8 @@ public class AccountDTO {
     private String city;
     private Integer age;
     private String postalCode;
+
+    @JsonBackReference // This field is not going to be serialized.
     private UserDTO user;
 
 }
