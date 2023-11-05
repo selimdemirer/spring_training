69ab615e16b56dbd100baede1f46deec10441fb6
selimdemirer <selimmdemirerr@gmail.com>
2023-11-05 12:58:48 +0300

    Module-18 School Lab is created!

--- src/main/java/com/cydeo/dto/AddressDTO.java
+++ src/main/java/com/cydeo/dto/AddressDTO.java
@@ -1,6 +1,7 @@
 package com.cydeo.dto;
 
 import com.cydeo.enums.AddressType;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,10 +23,13 @@ public class AddressDTO {
 
     private AddressType addressType;
 
+    @JsonBackReference(value = "student-address-reference") // defaultReference
     private StudentDTO student;
 
+    @JsonBackReference(value = "parent-address-reference") // defaultReference
     private ParentDTO parent;
 
+    @JsonBackReference(value = "teacher-address-reference") // defaultReference
     private TeacherDTO teacher;
 
     private Integer currentTemperature;
