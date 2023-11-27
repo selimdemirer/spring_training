d7bc6e7e27475678c4ab9d77aa461e970a6a53af
selimdemirer <selimmdemirerr@gmail.com>
2023-11-27 06:43:05 +0300

    Custom Annotation with "afterreturning" and "afterthrowing" are added into the LoggingAspect class

--- spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
+++ spring-21-AOP/src/main/java/com/cydeo/aspect/LoggingAspect.java
@@ -1,12 +1,14 @@
 package com.cydeo.aspect;
 
+import com.cydeo.dto.CourseDTO;
 import org.aspectj.lang.JoinPoint;
-import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
 import org.slf4j.Logger; //simple logging fasat for java
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Aspect
 @Component
@@ -15,7 +17,9 @@ public class LoggingAspect {
     // Logger: to put some information in console
     Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
 
+
         // EXECUTION EXAMPLE
+
 //    @Pointcut("execution(* com.cydeo.controller.CourseController.*(..))") //includes all methods in the CourseController
 //    public void myPointcut(){}
 //
@@ -28,7 +32,10 @@ public class LoggingAspect {
 //    public void log() {
 //        logger.info("Info log..........");
 //    }
-        // JOINTPOINT EXAMPLE
+
+
+        // JOINPOINT EXAMPLE
+
 //    @Pointcut("execution(* com.cydeo.repository.CourseRepository.findById(*))") //it takes only one parameter
 //    public void courseRepositoryFindByIdPC() {}
 //
@@ -37,7 +44,10 @@ public class LoggingAspect {
 //        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
 //                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
+
+
         // WITHIN AND @WITHIN EXAMPLE
+
 //    @Pointcut("within(com.cydeo.controller..*)")
 //    public void anyControllerOperation() {}
 //
@@ -49,7 +59,10 @@ public class LoggingAspect {
 //        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
 //                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
+
+
         // ANNOTATION EXAMPLE
+
 //    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)") // It only cares "DeleteMapping" methods
 //    public void anyDeleteControllerOperation() {}
 //
@@ -58,7 +71,10 @@ public class LoggingAspect {
 //        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
 //                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
-        //
+
+
+        // CUSTOM ANNOTATION EXAMPLE : You can define your methods using only one annotation => @LoggingAnnotation
+
 //    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
 //    public void loggingAnnotationPC() {}
 //
@@ -67,51 +83,63 @@ public class LoggingAspect {
 //        logger.info("Before -> Method: {}, Arguments: {}, Target: {}"
 //                , joinPoint.getSignature(), joinPoint.getArgs(), joinPoint.getTarget());
 //    }
-//
-//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+
+
+        // CUSTOM ANNOTATION WITH "AFTER RETURNING" EXAMPLES
+
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)") // It only cares "GetMapping" methods
 //    public void afterReturningGetMappingAnnotation() {}
-//
+
 //    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "result")
 //    public void afterReturningGetMappingOperation(JoinPoint joinPoint, Object result) {
 //        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), result.toString());
 //    }
-//
+
 //    @AfterReturning(pointcut = "afterReturningGetMappingAnnotation()", returning = "results")
 //    public void afterReturningGetMappingOperation(JoinPoint joinPoint, List<CourseDTO> results) {
 //        logger.info("After Returning -> Method: {}, Result: {}", joinPoint.getSignature(), results.toString());
 //    }
-//
-//    CourseDTO -> Object     --> This is ok
-//    List<CourseDTO> -> List<Object>     --> This is not ok
-//
+
+/*    CourseDTO -> Object     --> This is ok
+    List<CourseDTO> -> List<Object>     --> This is not ok    */
+
+
+            // CUSTOM ANNOTATION WITH "AFTER THROWING" EXAMPLE
+
+//    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)") // It only cares "GetMapping" methods
+//    public void afterReturningGetMappingAnnotation() {}
+
 //    @AfterThrowing(pointcut = "afterReturningGetMappingAnnotation()", throwing = "exception")
-//    public void afterThrowingGetMappingOperation(JoinPoint joinPoint, RuntimeException exception) {
+//    public void afterThrowingGetMappingOperation(JoinPoint joinPoint, RuntimeException exception) { // If getAllCourses method works perfectly, I won't see anything in the console
 //        logger.error("After Throwing -> Method: {}, Exception: {}"
-//                , joinPoint.getSignature().toShortString(), exception.getMessage());
+//                , joinPoint.getSignature().toShortString(), exception.getMessage()); // toShortString: it will show only method name
 //    }
-//
-//    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
-//    public void loggingAnnotationPC() {}
-//
-//    @Around("loggingAnnotationPC()")
-//    public Object anyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJoinPoint) {
-//
-//        logger.info("Before -> Method: {} - Parameter {}"
-//                , proceedingJoinPoint.getSignature().toShortString(), proceedingJoinPoint.getArgs());
-//
-//        Object result = null;
-//
-//        try {
-//            result = proceedingJoinPoint.proceed();
-//        } catch (Throwable throwable) {
-//            throwable.printStackTrace();
-//        }
-//
-//        logger.info("After -> Method: {} - Result: {}"
-//                , proceedingJoinPoint.getSignature().toShortString(), result.toString());
-//        return result;
-//
-//    }
+
+
+        // CUSTOM ANNOTATION WITH "AROUND" EXAMPLE
+
+    @Pointcut("@annotation(com.cydeo.annotation.LoggingAnnotation)")
+    public void loggingAnnotationPC() {}
+
+    @Around("loggingAnnotationPC()")
+    public Object anyLoggingAnnotationOperation(ProceedingJoinPoint proceedingJoinPoint) {
+
+        logger.info("Before -> Method: {} - Parameter {}"
+                , proceedingJoinPoint.getSignature().toShortString(), proceedingJoinPoint.getArgs());
+
+        Object result = null;
+
+        try {
+            result = proceedingJoinPoint.proceed();
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
+        }
+
+        logger.info("After -> Method: {} - Result: {}"
+                , proceedingJoinPoint.getSignature().toShortString(), result.toString());
+        return result;
+
+    }
 
 
 }
